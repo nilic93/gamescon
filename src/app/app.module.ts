46853461bcad3c143c6e44d196549617fbba8934
nikola <korbisin@gmail.com>
2020-03-29 19:29:52 +0200

    improved login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './routes/login/login.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { PacmanComponent } from './routes/components/pacman/pacman.component';
-import { MemorigyComponent } from './routes/components/memorigy/memorigy.component';
-import { MatchitComponent } from './routes/components/matchit/matchit.component';
-import { CatchitComponent } from './routes/components/catchit/catchit.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './routes/login/login.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {PacmanComponent} from './routes/components/pacman/pacman.component';
+import {MemorigyComponent} from './routes/components/memorigy/memorigy.component';
+import {MatchitComponent} from './routes/components/matchit/matchit.component';
+import {CatchitComponent} from './routes/components/catchit/catchit.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -20,13 +25,19 @@ import { CatchitComponent } from './routes/components/catchit/catchit.component'
     PacmanComponent,
     MemorigyComponent,
     MatchitComponent,
-    CatchitComponent
+    CatchitComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  username = new FormControl('', Validators.required);
+  email = new FormControl('', Validators.required);
+  password = new FormControl('', Validators.required);
+  repeatedPassword = new FormControl('', Validators.required);
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      username: this.username,
+      email: this.email,
+      password: this.password,
+      repeatedPassword: this.repeatedPassword,
+    });
+  }
+
+  onSubmit() {
+    console.log('model-based form submitted');
+    console.log(this.form);
+    console.log(this.form.value.firstName);
+  }
 
   ngOnInit(): void {
   }
